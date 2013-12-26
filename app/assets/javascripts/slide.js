78cb75360cb8a3b1d2cd3c6c8c6789ffbd8ea83f
刘晓琴 <liuxiaoqin@ebupt.com>
2013-12-26 12:58:32 +0800

    添加

---
+++ app/assets/javascripts/slide.js
@@ -0,0 +1,61 @@
+﻿var nBannerSizeL;
+$(function(){
+	
+	
+	var slideTimeL=400, slideTimeS=600, pics=3, curIndex=1, nBannerSizeS=219, oBannerS=$("#phoneUiList");
+	window.onresize=function(){
+		oBannerL.css("marginLeft","0px");
+		oBannerS.css("marginLeft","0px");
+		fnResize();
+	}
+	
+	function fnSlideLeft(){
+		console.log(curIndex);
+		if(curIndex<pics-1){
+			oBannerL.animate({marginLeft:"-="+nBannerSizeL},slideTimeL);
+			oBannerS.animate({marginLeft:"-="+nBannerSizeS},slideTimeS);
+			curIndex++;
+		}else{
+			oBannerL.animate({marginLeft:"0px"},slideTimeL);
+			oBannerS.animate({marginLeft:"0px"},slideTimeS);
+			curIndex=0;
+		}
+	}
+	
+	function fnSlideRight(){
+		if(curIndex>0){
+			oBannerL.animate({marginLeft:"+="+nBannerSizeL},slideTimeL);
+			oBannerS.animate({marginLeft:"+="+nBannerSizeS},slideTimeS);
+			curIndex--;
+		}else{
+			oBannerL.animate({marginLeft:-nBannerSizeL*(pics-1)+"px"},slideTimeL);
+			oBannerS.animate({marginLeft:-nBannerSizeS*(pics-1)+"px"},slideTimeS);
+			curIndex=pics-1;
+		}
+	}
+	
+	var slideAni=setInterval(fnSlideLeft,6640);
+	
+	$("#leftBtn").click(function(){
+		clearInterval(slideAni);
+		slideAni=setInterval(fnSlideLeft,6640);
+		if(oBannerL.is(":animated") || oBannerS.is(":animated")){
+			oBannerL.stop(true,true);
+			oBannerS.stop(true,true);
+		}else{
+			fnSlideRight();
+		}
+	});
+	
+	$("#rightBtn").click(function(){
+		clearInterval(slideAni);
+		slideAni=setInterval(fnSlideLeft,6640);
+		if(oBannerL.is(":animated") || oBannerS.is(":animated")){
+			oBannerL.stop(true,true);
+			oBannerS.stop(true,true);
+		}else{
+			fnSlideLeft();
+		}
+	});
+	
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RockNet::Application.config.secret_token = 'fce4957cba3d5e72cdb1e2aef8ef90a24b148f5c6b989ff537e97d373954fca1099c8f26f05be234daa0c170c600dfa7d464fb6f6efd1c0202c9a512a00eb04b'
