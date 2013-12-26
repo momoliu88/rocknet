08aba7c6f827f7b57aa40ad734a4711bc70b83b7
刘晓琴 <liuxiaoqin@ebupt.com>
2013-12-26 23:51:38 +0800

    fix error

--- app/assets/javascripts/myself.js
+++ app/assets/javascripts/myself.js
@@ -7,7 +7,7 @@ var options = {
 	totalPages: Math.ceil(size*1.0/9.0),
 	onPageClicked: function(e,originalEvent,type,page){
 	//	$.get("/product_type/products?pages="+page+"&protype="+protype1);
-	//$.get("/product_type/products",{pages:page,protype:proT});
+	$.get("/product_type/products",{pages:page,protype:proT});
 		},
 };
 $('#pag').bootstrapPaginator(options);
